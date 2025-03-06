6e9625073b947a507100ae1201bbca0609b89d16
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2025-03-06 11:28:05 +0530

    Add Prim's Algorithm implementation for minimum spanning tree

---
+++ Day-4/PrimAlgorithm.java
@@ -0,0 +1,67 @@
+//! wap to perform  minimum spaning tree using primis algorithm
+
+public class PrimAlgorithm {
+    public static void main(String[] args) {
+        int[][] graph = {
+            {0, 2, 0, 6, 0},
+            {2, 0, 3, 8, 5},
+            {0, 3, 0, 0, 7},
+            {6, 8, 0, 0, 9},
+            {0, 5, 7, 9, 0}
+        };
+        primMST(graph);
+    }
+
+    public static void primMST(int[][] graph) {
+        int V = graph.length;
+        int[] parent = new int[V];
+        int[] key = new int[V];
+        boolean[] mstSet = new boolean[V];
+
+        for (int i = 0; i < V; i++) {
+            key[i] = Integer.MAX_VALUE;
+            mstSet[i] = false;
+        }
+
+        key[0] = 0;
+        parent[0] = -1;
+
+        for (int count = 0; count < V - 1; count++) {
+            int u = minKey(key, mstSet);
+            mstSet[u] = true;
+
+            for (int v = 0; v < V; v++) {
+                if (graph[u][v] != 0 && !mstSet[v] && graph[u][v] < key[v]) {
+                    parent[v] = u;
+                    key[v] = graph[u][v];
+                }
+            }
+        }
+
+        printMST(parent, graph);
+    }
+
+    public static int minKey(int[] key, boolean[] mstSet) {
+        int min = Integer.MAX_VALUE, minIndex = -1;
+
+        for (int v = 0; v < key.length; v++) {
+            if (!mstSet[v] && key[v] < min) {
+                min = key[v];
+                minIndex = v;
+            }
+        }
+
+        return minIndex;
+    }
+
+    public static void printMST(int[] parent, int[][] graph) {
+        System.out.println("Edge \tWeight");
+        for (int i = 1; i < graph.length; i++) {
+            System.out.println(parent[i] + " - " + i + "\t" + graph[i][parent[i]]);
+        }
+    }
+}
+
+
+//? Time Complexity: O(V^2)
+//? Space Complexity: O(V)
