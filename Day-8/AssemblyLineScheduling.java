7aff7467250c8249a582023ad467b05f15639e21
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2025-03-26 18:59:55 +0530

    Refactor carAssembly method for improved readability and streamline main method initialization

--- Day-8/AssemblyLineScheduling.java
+++ Day-8/AssemblyLineScheduling.java
@@ -2,35 +2,23 @@
 
 class AssemblyLineScheduling {
     static int carAssembly(int a[][], int t[][], int e[], int x[], int n) {
-        int first, second;
-  
-        first = e[0] + a[0][0];
-        second = e[1] + a[1][0];
-  
+        int first = e[0] + a[0][0], second = e[1] + a[1][0];
         for (int i = 1; i < n; i++) {
             int up = Math.min(first + a[0][i], second + t[1][i] + a[0][i]);
             int down = Math.min(second + a[1][i], first + t[0][i] + a[1][i]);
             first = up;
             second = down;
         }
-  
         return Math.min(first + x[0], second + x[1]);
     }
-  
+
     public static void main(String[] args) {
         int a[][] = { { 4, 5, 3, 2 }, { 2, 10, 1, 4 } };
         int t[][] = { { 0, 7, 4, 5 }, { 0, 9, 2, 8 } };
-        int e[] = { 10, 12 }; 
-        int x[] = { 18, 7 };  
-        int n = a[0].length;
-  
-        System.out.println("Minimum time required: " + carAssembly(a, t, e, x, n));
+        int e[] = { 10, 12 }, x[] = { 18, 7 };
+        System.out.println("Minimum time required: " + carAssembly(a, t, e, x, a[0].length));
     }
 }
-
-
-
-
 
 
 // ? To perform enter the following command:
