b6c27509944ef4901b7b6f04da75a0dec2619149
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2025-03-26 16:19:56 +0530

    Simplify optimalMerge method by reducing comments and consolidating file addition to priority queue

--- Day-7/OptimalMergeSort.java
+++ Day-7/OptimalMergeSort.java
@@ -4,32 +4,14 @@ import java.util.PriorityQueue;
 
 class OptimalMergeSort {
     static int optimalMerge(int[] files) {
-        // Create a priority queue to store the file sizes
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        
-        // Add all file sizes to the priority queue
-        for (int file : files) {
-            pq.add(file);
-        }
-        
+        for (int file : files) pq.add(file);
         int totalCost = 0;
-        
-        // While there is more than one file in the queue
         while (pq.size() > 1) {
-            // Remove the two smallest files
-            int first = pq.poll();
-            int second = pq.poll();
-            
-            // Merge the two files
-            int mergeCost = first + second;
-            
-            // Add the merge cost to the total cost
+            int mergeCost = pq.poll() + pq.poll();
             totalCost += mergeCost;
-            
-            // Add the merged file back to the queue
             pq.add(mergeCost);
         }
-        
         return totalCost;
     }
 
