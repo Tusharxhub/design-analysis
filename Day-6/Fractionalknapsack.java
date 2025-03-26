bbaf5800512988bfe0e5a887c8708b3a421bdeac
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2025-03-26 11:25:59 +0530

    Refactor Fractional Knapsack implementation for improved readability and efficiency

--- Day-6/Fractionalknapsack.java
+++ Day-6/Fractionalknapsack.java
@@ -1,7 +1,6 @@
 //! wap in java to perform fractional knapsack
 
 import java.util.Arrays;
-import java.util.Comparator;
 
 class Item {
     int weight, value;
@@ -14,35 +13,20 @@ class Item {
 public class Fractionalknapsack {
 
     static double getMaxValue(int W, Item[] items) {
-        Arrays.sort(items, (o1, o2) -> {
-            double r1 = (double) o1.value / o1.weight;
-            double r2 = (double) o2.value / o2.weight;
-            return Double.compare(r2, r1);
-        });
-
-        double totalValue = 0d;
-
+        Arrays.sort(items, (a, b) -> Double.compare((double) b.value / b.weight, (double) a.value / a.weight));
+        double totalValue = 0;
         for (Item item : items) {
-            int curWt = item.weight;
-            int curVal = item.value;
-
-            if (W - curWt >= 0) {
-                W -= curWt;
-                totalValue += curVal;
-            } else {
-                double fraction = ((double) W / (double) curWt);
-                totalValue += (curVal * fraction);
-                break;
-            }
+            if (W == 0) break;
+            int curWt = Math.min(item.weight, W);
+            totalValue += curWt * ((double) item.value / item.weight);
+            W -= curWt;
         }
-
         return totalValue;
     }
 
     public static void main(String[] args) {
         Item[] items = { new Item(10, 60), new Item(20, 100), new Item(30, 120) };
         int W = 50;
-
         System.out.println("Maximum value we can obtain = " + getMaxValue(W, items));
     }
 }
