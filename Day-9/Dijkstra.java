3844cd943f1161f9ef6adc643d60607eabb1a48b
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2025-03-26 19:20:03 +0530

    Refactor Dijkstra's algorithm implementation to use adjacency matrix and simplify edge handling

--- Day-9/Dijkstra.java
+++ Day-9/Dijkstra.java
@@ -1,81 +1,46 @@
 //! wap in java to implement Dijkstra's algorithm
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Comparator;
 
-class Edge {
-    int src, dest, weight;
-    public Edge(int src, int dest, int weight) {
-        this.src = src;
-        this.dest = dest;
-        this.weight = weight;
-    }
-}
-
-class Node implements Comparable<Node> {
-    int vertex, weight;
-    public Node(int vertex, int weight) {
-        this.vertex = vertex;
-        this.weight = weight;
-    }
-    public int compareTo(Node node) {
-        return this.weight - node.weight;
-    }
-}
-
-class Graph {
-    int vertices;
-    List<List<Edge>> adjList;
-
-    public Graph(int vertices) {
-        this.vertices = vertices;
-        adjList = new ArrayList<>(vertices);
-        for (int i = 0; i < vertices; i++) adjList.add(new ArrayList<>());
-    }
-
-    public void addEdge(int src, int dest, int weight) {
-        adjList.get(src).add(new Edge(src, dest, weight));
-        adjList.get(dest).add(new Edge(dest, src, weight));
-    }
-
-    public void dijkstra(int start) {
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        int[] dist = new int[vertices];
+class Dijkstra {
+    static void dijkstra(int[][] graph, int src) {
+        int V = graph.length;
+        int[] dist = new int[V];
+        boolean[] visited = new boolean[V];
         Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[start] = 0;
-        pq.add(new Node(start, 0));
+        dist[src] = 0;
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
+        pq.offer(new int[]{src, 0});
 
         while (!pq.isEmpty()) {
-            Node node = pq.poll();
-            int vertex = node.vertex;
+            int u = pq.poll()[0];
+            if (visited[u]) continue;
+            visited[u] = true;
 
-            for (Edge edge : adjList.get(vertex)) {
-                int newDist = dist[vertex] + edge.weight;
-                if (newDist < dist[edge.dest]) {
-                    dist[edge.dest] = newDist;
-                    pq.add(new Node(edge.dest, newDist));
+            for (int v = 0; v < V; v++) {
+                if (graph[u][v] > 0 && !visited[v] && dist[u] + graph[u][v] < dist[v]) {
+                    dist[v] = dist[u] + graph[u][v];
+                    pq.offer(new int[]{v, dist[v]});
                 }
             }
         }
 
-        printDijkstra(dist, start);
-    }
-
-    public void printDijkstra(int[] dist, int start) {
-        System.out.println("Dijkstra's Algorithm: (Start Vertex: " + start + ")");
-        for (int i = 0; i < dist.length; i++) {
-            System.out.println("Vertex " + i + " is at distance " + dist[i]);
-        }
+        System.out.println("Vertex \t Distance from Source");
+        for (int i = 0; i < V; i++)
+            System.out.println(i + " \t " + dist[i]);
     }
 
     public static void main(String[] args) {
-        Graph graph = new Graph(6);
-        graph.addEdge(0, 1, 4);
-        graph.addEdge(0, 2, 3);
-        graph.addEdge(1, 2, 1);
-        graph.addEdge(1, 3, 2);
-        graph.addEdge(2, 3, 4);
-        graph.addEdge(3, 4, 2);
-        graph.addEdge(4, 5, 6);
-        graph.dijkstra(0);
+        int[][] graph = {
+            {0, 10, 0, 30, 100},
+            {10, 0, 50, 0, 0},
+            {0, 50, 0, 20, 10},
+            {30, 0, 20, 0, 60},
+            {100, 0, 10, 60, 0}
+        };
+        dijkstra(graph, 0); 
     }
 }
